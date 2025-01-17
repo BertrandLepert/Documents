d09e4418abba866fc5b6816087ee381191843cf9
BertrandLepert <lepert.bertrand@gmail.com>
2025-01-17 15:53:09 +0100

    avancée script

--- javascrip_tfirst_stape/script.js
+++ javascrip_tfirst_stape/script.js
@@ -1,1 +1,54 @@
-console.log("Hello World");
+let score = 0
+
+const listeMots = ['Cachalot', 'Pétunia', 'Serviette']
+const listephrases = ["pas de panique", "la vie l'univers et le reste", "merci pour le poisson"]
+
+let Total = listeMots.length
+
+choix = choisirPhraseOuMots()
+
+if (choix === "mots"){
+    for(let compteur =0; compteur < 3; compteur++){
+    
+        let motUtilisateur = prompt("Entrez le mot : " + listeMots[compteur])
+        
+        if (motUtilisateur === listeMots[compteur]) {
+            console.log("Bravo !")
+            score++
+        } else {
+            console.log("Vous avez fait une erreur de frappe!!!")
+        }
+    }
+    console.log(score)
+} else {
+    for(let compteur =0; compteur < 3; compteur++){
+    
+        let motUtilisateur = prompt("Entrez le mot : " + listephrases[compteur])
+        
+        if (motUtilisateur === listephrases[compteur]) {
+            console.log("Bravo !")
+            score++
+        } else {
+            console.log("Vous avez fait une erreur de frappe!!!")
+        }
+    }
+    console.log(score)
+}
+
+function afficherResultat(score,Total){
+    
+    prompt("le résultat est de " + score +" sur " + Total)
+
+    return 0
+}
+
+function choisirPhraseOuMots(){
+    
+    let choix = prompt("Veuillez choisir la liste mots ou phrases")
+
+    while(choix !== "mots" && choix !== "phrases"){
+        choix = prompt("Veuillez choisir la liste mots ou phrases")
+    }
+
+    return choix
+}
